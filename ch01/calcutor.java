a653eeaf63b939d6303c1057e54c9792ea4d3130
bomin <yk051016@gmail.com>
2024-10-15 21:00:57 +0900

    테스트

---
+++ ch01/calcutor.java
@@ -0,0 +1,8 @@
+public class calcutor {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int result = x+y; //변수 result , x + y 값 저장//
+        System.out.println(result);
+    }
+}
