c57527663631ea50936456d86f49cd17ad02c812
bomin <yk051016@gmail.com>
2024-11-05 20:19:28 +0900

    중첩선언/익명객체

--- ch09/sec02/exam02/HomeExam.java
+++ ch09/sec02/exam02/HomeExam.java
@@ -2,16 +2,16 @@ package ch09.sec02.exam02;
 
 public class HomeExam {
     public static void main(String[] ars) {
-        //Home 객체생성//
+        //Home : 객체생성//
         Home home = new Home();
         
-        //익명 구현 객체가 대입된 필드//
+        //익명 구현 객체 대입 [필드]//
         home.use1();
         
-        //익명 구현 객체가 대입된 로컬변수//
+        //익명 구현 객체 대입 [로컬변수]//
         home.use2();
 
-        //익명 구현 객체가 대입된 매개변수//
+        //익명 구현 객체 대입 [매개변수]//
         home.use3(new RemoteControl() {
             @Override
             public void turnOn() {
